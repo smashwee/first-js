77874bde8a83ee9e4299e0b355b25e2313d6acce
Ashley Cacoilo <acacoilo926@gmail.com>
2018-01-25 20:58:57 -0500

    added in class exercise to main.js

--- js/main.js
+++ js/main.js
@@ -29,3 +29,49 @@ var animals = ['dog', 'cat', 'bird', 'fish']
 
 console.log(colors[1],numbers[2]);
 console.log(animals[3]);
+
+//Multi-dimesional array
+var meat = ['ham', 'bologna', 'turkey'];
+var fruit = ['oranges', 'apples', 'grapes'];
+var food = [meat, fruit];
+
+console.log(food[1][0]);// logs 'oranges' to the console
+
+// var food = [['ham'], ['oranges']]; // could also be written like this
+
+// object:
+var user = {
+  firstName: 'Lucille',
+  age: 40,
+  lastName: 'Bluth',
+  hasHair: true,
+  'has-feet': true,
+};
+
+// access properties of our user obj using dot notation
+console.log(user.firstName); // This will log 'Lucille'
+// we can also access properties on objs using bracket notation
+console.log(user['firstName']);// This will also log 'Lucille'
+console.log(user['has-feet']);// NEED to use square bracket
+
+// two vars, each containing a #
+var number1 = 20;
+var number2 = 25;
+alert('the result is: ' + (number1 - number2) + ' oops');
+
+var me = {
+  firstName: 'Ashley',
+  lastName: 'Cacoilo',
+  fingerCount: 10,
+  interests: ['video games', 'being crafty', 'chocolate']
+};
+
+console.log(me.firstName);
+console.log(me.lastName);
+console.log(me.fingerCount);
+console.log(me.interests[0]);
+
+var string1 = 'Ashley';
+var string2 = 'Cacoilo';
+
+alert(string1 + ' ' + string2);
