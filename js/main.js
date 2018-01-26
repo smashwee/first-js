8052f0b7f1e9325d95e3920687d068a20ed38653
Ashley Cacoilo <acacoilo926@gmail.com>
2018-01-25 20:03:22 -0500

    logging some things to the console in main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ function change (){
 var name = 'Lucille'; // Store a string in a var
 var numberOfWidgets = 10; // Number
 var isCodingCool = true; // boolean
+
+// store an array in a var
+var characters = [50, true, 'Lucille', 'Michael Bluth', 'Tobias', 10, 2];
+console.log(characters[0]);
+
+/* create some variables,
+store some data in them,
+output them to the console. */
+
+var colors = ['green', 'blue', 'red', 'purple', 'pink'];
+var numbers = [12, 34, 86, 23];
+var animals = ['dog', 'cat', 'bird', 'fish']
+
+console.log(colors[1],numbers[2]);
+console.log(animals[3]);
